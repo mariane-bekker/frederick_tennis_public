518d92ca363c7934b10d928e4184daf61e897270
Max Thom Stahl <max.stahl@mindbodyonline.com>
2020-02-18 13:46:10 -0800

    Newer readme description, fixed a spec

--- spec/requests/games_requests_spec.rb
+++ spec/requests/games_requests_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :request do
-  let(:server_name) { 'Jello Biafra' }
-  let(:receiver_name) { 'East Bay Ray' }
+  let(:server_name) { 'Venus' }
+  let(:receiver_name) { 'Serena' }
   let(:request_headers) { { 'Content-Type' => 'application/json' } }
 
   let(:game) { Game.create!(server_name: server_name, receiver_name: receiver_name) }
@@ -12,22 +12,21 @@ RSpec.describe GamesController, type: :request do
       [0, 0, 'Love all'],
       [1, 1, '15 all'],
       [2, 2, '30 all'],
-      [3, 3, '40 all'],
+      [3, 3, 'Deuce'],
       [4, 4, 'Deuce'],
       [0, 1, 'Love 15'],
       [1, 0, '15 love'],
       [13, 13, 'Deuce'],
-      [19, 18, 'Advantage out'],
+      [19, 20, 'Advantage out'],
       [5, 4, 'Advantage in'],
+      [4, 3, 'Advantage in'],
       [3, 1, '40 10'],
-      [5, 3, 'Game Jello Biafra'],
-      [5, 7, 'Game East Bay Ray'],
+      [5, 3, 'Game Venus'],
+      [5, 7, 'Game Serena'],
     ].each do |test_case|
       server_score, receiver_score, called_score = test_case
 
       it "correctly scores #{server_score}-#{receiver_score} as #{called_score}" do
-        # response = nil
-
         server_score.times do
           post "/games/#{game.id}/score", params: server_name, headers: request_headers
           expect(response).to be_success
@@ -42,7 +41,6 @@ RSpec.describe GamesController, type: :request do
 
         parsed_response = JSON.parse(response.body)
 
-        # Expect the last request to have returned the final score we're expecting
         expect(parsed_response['called_score']).to eq called_score
       end
     end
